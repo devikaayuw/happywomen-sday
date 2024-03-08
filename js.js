517cd64379158bb6df0933d125bd06e7201e561a
Devika Ayu Wardani <142604971+devikaayuw@users.noreply.github.com>
2024-03-08 17:50:31 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,4 @@
+
+function showWish() {
+    alert("Happy Women's Day! ( ̳• · • ̳) / づ♡ Sending you lots of love and good vibes ! Terimakasih sudah menjadi teman yang baik, rajin nan unik. Semoga skripsi kita lancar, lulus bareng, suskes bareng ya! See you on top :) ! xoxo, depica");
+        }
